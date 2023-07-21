406ddb5990f63e67e1c72065190adb789ddcb7cc
rick0693 <108191105+rick0693@users.noreply.github.com>
2023-07-21 00:59:10 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -89,24 +89,51 @@ def botmail():
             st.warning('Insira um destinatário válido.')
 
 def bozap():
-    st.title('Bozap')
-    # Conteúdo da aba "Bozap"
-    st.write("Conteúdo da aba Bozap.")
+    st.title("Bozap")
+    st.write("Bem-vindo(a) à aba Bozap!")
+    st.write("Aqui você pode gerar uma mensagem para WhatsApp ou enviar um e-mail.")
 
+    # Geração de mensagem para WhatsApp
+    mensagem_gerada = gerar_mensagem()
+
+    st.markdown("**Texto gerado para WhatsApp:**")
+    st.text_area("", mensagem_gerada, height=200, max_chars=None, key=None)
+
+    numero_whatsapp = st.text_input("Insira o número de WhatsApp do destinatário (inclua o DDI, ex.: +55):")
+
+    if st.button("Gerar Link para WhatsApp") and mensagem_gerada:
+        mensagem_codificada = quote(mensagem_gerada)
+        link_whatsapp = f"https://wa.me/{numero_whatsapp}?text={mensagem_codificada}"
+        st.markdown(f"**Link para enviar a mensagem:**")
+        st.markdown(f"[{link_whatsapp}]({link_whatsapp})")
+
+    # Envio de e-mail
+    st.markdown("---")
+    st.write("Envie o texto acima como um e-mail:")
+    
+    destinatario_email = st.text_input("Insira o e-mail do destinatário:")
+    assunto_email = st.text_input("Insira o assunto do e-mail:")
+    corpo_email = st.text_area("Insira o corpo do e-mail:")
+
+    uploaded_file = st.file_uploader('Selecione um arquivo para anexar', type=['pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+    if st.button("Enviar E-mail") and destinatario_email.strip() != '':
+        arquivo_anexo = uploaded_file if uploaded_file is not None else None
+        enviar_email(destinatario_email, assunto_email, corpo_email, arquivo_anexo)
+        st.success('E-mail enviado com sucesso!')
+
+# Função principal da aba "BotBlaze"
 def botBlaze():
-    st.title('BotBlaze')
-    # Conteúdo da aba "BotBlaze"
+    st.title("BotBlaze")
     st.write("Conteúdo da aba BotBlaze.")
 
 # Definindo o layout da página com a sidebar e as abas
 def main():
     st.sidebar.title('Menu')
-    opcoes = ['Botmail', 'Bozap', 'BotBlaze']
+    opcoes = ['Bozap', 'BotBlaze']
     aba_selecionada = st.sidebar.selectbox('Selecione a aba:', opcoes)
 
-    if aba_selecionada == 'Botmail':
-        botmail()
-    elif aba_selecionada == 'Bozap':
+    if aba_selecionada == 'Bozap':
         bozap()
     elif aba_selecionada == 'BotBlaze':
         botBlaze()
